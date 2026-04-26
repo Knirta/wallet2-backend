7c88bb2df1012f370f242826d3ecb1670968c0ff
Knirta <myknirtak@gmail.com>
2026-04-26 18:57:43 +0300

    added transaction model

---
+++ src/models/transaction.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+import { handleMongooseError } from "../helpers/index.js";
+
+const transactionSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+      index: true,
+    },
+    sum: { type: Number, required: true },
+    type: { type: String, enum: ["income", "expense"], required: true },
+    category: { type: String, required: true },
+    date: { type: Date, required: true },
+    comment: { type: String, default: "" },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+transactionSchema.post("save", handleMongooseError);
+
+const createTransactionSchema = Joi.object({
+  sum: Joi.number().integer().positive().required().messages({
+    "number.integer": "Сума повинна бути цілим числом",
+    "number.positive": "Сума повинна бути додатнім числом",
+    "any.required": "Сума є обов'язковою для заповнення",
+  }),
+  type: Joi.string().valid("income", "expense").required(),
+  category: Joi.string().required(), //todo --- add id ---
+  date: Joi.date().required(),
+  comment: Joi.string().allow("").max(250).optional(),
+});
+
+const schemas = { createTransactionSchema };
+
+const Transaction = model("transaction", transactionSchema);
+
+export { Transaction, schemas };
