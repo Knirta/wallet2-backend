57713a8518bc5a92fb9422a329cf91d2b125f5d7
Knirta <myknirtak@gmail.com>
2026-04-30 11:07:24 +0300

    categories ctrl: expanded server response

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -3,7 +3,14 @@ import { ctrlWrapper } from "../helpers/index.js";
 
 const getCategories = async (req, res) => {
   const categories = await Category.find({});
-  res.json(categories);
+  res.status(200).json({
+    status: "success",
+    code: 200,
+    message: "Категорії успішно отримані",
+    data: {
+      categories,
+    },
+  });
 };
 
 export default {
