56adda8badbf73a35b7467a08d6dc1a48e75295a
Knirta <myknirtak@gmail.com>
2026-07-20 21:07:48 +0300

    deleted category colors from db

--- src/seaders/categorySeader.js
+++ src/seaders/categorySeader.js
@@ -1,47 +1,41 @@
 import { Category } from "../models/category.js";
 
 const defaultCategories = [
-  { name: "Зарплата", type: "income", icon: "salary", color: "#4CAF50" },
-  { name: "Подарунки", type: "income", icon: "gift", color: "#2196F3" },
-  { name: "Фріланс", type: "income", icon: "freelance", color: "#FF9800" },
+  { name: "Зарплата", type: "income", icon: "salary" },
+  { name: "Подарунки", type: "income", icon: "gift" },
+  { name: "Фріланс", type: "income", icon: "freelance" },
   {
     name: "Інші доходи",
     type: "income",
     icon: "other-income",
-    color: "#9C27B0",
   },
-  { name: "Продукти", type: "expense", icon: "groceries", color: "#FF5722" },
+  { name: "Продукти", type: "expense", icon: "groceries" },
   {
     name: "Транспорт",
     type: "expense",
     icon: "transportation",
-    color: "#795548",
   },
   {
     name: "Дозвілля",
     type: "expense",
     icon: "entertainment",
-    color: "#E91E63",
   },
   {
     name: "Комунальні послуги",
     type: "expense",
     icon: "utilities",
-    color: "#3F51B5",
   },
-  { name: "Здоров'я", type: "expense", icon: "health", color: "#009688" },
-  { name: "Освіта", type: "expense", icon: "education", color: "#673AB7" },
+  { name: "Здоров'я", type: "expense", icon: "health" },
+  { name: "Освіта", type: "expense", icon: "education" },
   {
     name: "Товари для дому",
     type: "expense",
     icon: "home-goods",
-    color: "#FF9800",
   },
   {
     name: "Інші витрати",
     type: "expense",
     icon: "other-expense",
-    color: "#607D8B",
   },
 ];
 
